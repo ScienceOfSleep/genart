413c3bdf5e7ffcafe22614c53049365eeddef438
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2021-01-12 19:05:30 -0700

    Created a template for LinkedIn cover photos with big circles. Either part can be split off in the future.

---
+++ li-banner-big-circles.js
@@ -0,0 +1,61 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
+
+const settings = {
+    dimensions: [ 1584, 396 ]
+};
+
+const sketch = () => {
+    // random.setSeed(13)
+    // const frequency = 0.5;
+    const palette = random.pick(palettes);
+
+    const createGrid = () => {
+        const points = [];
+        const count = 20;
+        for ( let x = 0; x<count; x++) {
+            for ( let y=0; y<count; y++) {
+                const u = count <= 1 ? 0.5 : (x / (count - 1));
+                const v = count <= 1 ? 0.5 : (y / (count - 1));
+                const radius = Math.abs(random.noise2D(u, v));
+                points.push({
+                    color: random.pick(palette),
+                    radius,
+                    position: [u,v]
+                });
+            }
+        }
+        return points;
+    };
+
+    const points = createGrid().filter(() => random.value() > 0.25);
+    const margin = 20;
+
+    return ({ context, width, height }) => {
+        context.fillStyle = '#ffffff';
+        context.fillRect(0, 0, width, height);
+
+        points.forEach(data => {
+            const {
+                position,
+                radius,
+                color,
+            } = data
+
+
+            const [u,v] = position
+
+            const x = lerp(margin, width - margin, u);
+            const y = lerp(margin, height - margin, v);
+
+            context.beginPath();
+            context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+            context.fillStyle = color;
+            context.fill();
+        });
+    };
+};
+
+canvasSketch(sketch, settings);
--- sketch.js
+++ sketch.js
@@ -1,43 +1,59 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1584, 396 ]
 };
 
 const sketch = () => {
+  // random.setSeed(13)
+  // const frequency = 0.5;
+  const palette = random.pick(palettes);
+
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 20;
     for ( let x = 0; x<count; x++) {
       for ( let y=0; y<count; y++) {
-        const u = x / (count - 1);
-        const v = y / (count - 1);
-        points.push([ u, v ])
+        const u = count <= 1 ? 0.5 : (x / (count - 1));
+        const v = count <= 1 ? 0.5 : (y / (count - 1));
+        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+        points.push({
+          color: random.pick(palette),
+          radius,
+          position: [u,v]
+      });
       }
     }
     return points;
   };
 
-  const points = createGrid();
-  const margin = 400;
+  const points = createGrid().filter(() => random.value() > 0.25);
+  const margin = 20;
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#ffffff';
     context.fillRect(0, 0, width, height);
-    context.strokeStyle = 'black';
-    context.lineWidth = 10;
-    context.strokeRect(0, 0, width, height);
 
-    points.forEach(([ u, v]) => {
+    points.forEach(data => {
+      const {
+        position,
+        radius,
+        color,
+      } = data
+
+
+      const [u,v] = position
+
       const x = lerp(margin, width - margin, u);
-      const y = lerp(margin, width - margin, v);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 200, 0, Math.PI * 1.3, false);
-      context.strokeStyle = 'black';
-      context.lineWidth = 20;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = color;
+      context.fill();
     });
   };
 };
